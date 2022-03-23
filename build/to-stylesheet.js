ff0c924ccb3630493e442ef401264cac5bf82647
Сергей Кацер <99019151+skarmind@users.noreply.github.com>
2022-03-23 14:47:54 +0300

    fix: update structure

--- build/to-stylesheet.js
+++ build/to-stylesheet.js
@@ -8,9 +8,6 @@ export const buildPropsStylesheet = (
 
   let appendedMeta = "";
 
-  if (filename.includes("shadows"))
-    file.write(`@import 'props.media.css';\n\n`);
-
   file.write(`${selector ? "[color-scheme='dark']" : ":root"} {\n`);
 
   Object.entries(props).forEach(([prop, val]) => {
@@ -18,23 +15,8 @@ export const buildPropsStylesheet = (
 
     if (prefix && prefix !== "''") prop = `--${prefix}-` + prop.slice(2);
 
-    if (prop.includes("animation")) {
-      let keyframes = props[`${prop}-@`];
-      appendedMeta += keyframes;
-    }
-
     file.write(`  ${prop}: ${val};\n`);
   });
-
-  if (filename.includes("shadows")) {
-    appendedMeta += `
-@media (--OSdark) {
-  :where(html) {
-    --shadow-strength: 25%;
-    --shadow-color: 220 40% 2%;
-  }
-}`;
-  }
 
   file.write("}\n");
   file.end(appendedMeta);
